b48eaf9c1b05b63c62cb25683fc99dfb37100d9d
scorcherfjk <franjavikale@gmail.com>
2020-12-31 12:03:49 -0500

    configuracion del proyecto

---
+++ Learning Core Data/Model/Person+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Person+CoreDataClass.swift
+//  Learning Core Data
+//
+//  Created by Francisco De Freitas on 30/12/20.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Person)
+public class Person: NSManagedObject {
+
+}
